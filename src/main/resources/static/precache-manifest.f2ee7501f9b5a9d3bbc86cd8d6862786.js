a67fabe15ed74a7980c7e59dc439fcab3457fdec
Marin Kos <kmarin@racunarstvo.hr>
2019-02-22 21:16:15 +0100

    added frontend

---
+++ src/main/resources/static/precache-manifest.f2ee7501f9b5a9d3bbc86cd8d6862786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86bb7550e7a8fd39d479",
+    "url": "/static/js/main.86bb7550.chunk.js"
+  },
+  {
+    "revision": "a2317a8edce79a37aab5",
+    "url": "/static/js/1.a2317a8e.chunk.js"
+  },
+  {
+    "revision": "86bb7550e7a8fd39d479",
+    "url": "/static/css/main.ca810577.chunk.css"
+  },
+  {
+    "revision": "a2317a8edce79a37aab5",
+    "url": "/static/css/1.dbb70bbd.chunk.css"
+  },
+  {
+    "revision": "38dd5d210b4b1f3400171564d5e49498",
+    "url": "/index.html"
+  }
+];
